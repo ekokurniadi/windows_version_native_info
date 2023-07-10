d1bc674dda191f73752055ca07cd567994bbe298
Eko Kurniadi <eko.kurniadi@majoo.id>
2023-07-10 14:35:54 +0700

    tidy up

--- test/windows_version_native_info_test.dart
+++ test/windows_version_native_info_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:windows_version_native_info/windows_version_native_info.dart';
 import 'package:windows_version_native_info/windows_version_native_info_platform_interface.dart';
-import 'package:windows_version_native_info/windows_version_native_info_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 class MockWindowsVersionNativeInfoPlatform
@@ -13,17 +11,5 @@ class MockWindowsVersionNativeInfoPlatform
 }
 
 void main() {
-  final WindowsVersionNativeInfoPlatform initialPlatform = WindowsVersionNativeInfoPlatform.instance;
-
-  test('$MethodChannelWindowsVersionNativeInfo is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelWindowsVersionNativeInfo>());
-  });
-
-  test('getPlatformVersion', () async {
-    WindowsVersionNativeInfo windowsVersionNativeInfoPlugin = WindowsVersionNativeInfo();
-    MockWindowsVersionNativeInfoPlatform fakePlatform = MockWindowsVersionNativeInfoPlatform();
-    WindowsVersionNativeInfoPlatform.instance = fakePlatform;
-
-    expect(await windowsVersionNativeInfoPlugin.getPlatformVersion(), '42');
-  });
+  
 }
