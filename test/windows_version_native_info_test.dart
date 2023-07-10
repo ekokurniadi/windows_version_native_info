031dfc87e48ce5d5d7f2bb77ab6b6bf7008727b9
Eko Kurniadi <eko.kurniadi@majoo.id>
2023-07-10 15:16:09 +0700

    add some readable message

--- test/windows_version_native_info_test.dart
+++ test/windows_version_native_info_test.dart
@@ -1,15 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:windows_version_native_info/windows_version_native_info_platform_interface.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-
-class MockWindowsVersionNativeInfoPlatform
-    with MockPlatformInterfaceMixin
-    implements WindowsVersionNativeInfoPlatform {
-
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
-
 void main() {
   
 }
